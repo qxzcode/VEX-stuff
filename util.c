9d64110566c870372b33d4a7fc08455437af1a79
qxzcode <qxzcraft@gmail.com>
2015-10-01 15:37:53 -0700

    First commit

---
+++ util.c
@@ -0,0 +1,22 @@
+bool turnFlywheelOn = false;
+word flywheelSpeed = 80;
+
+task flywheelTask() {
+	// monitor turnFlywheelOn and control the motor safely
+	word fwMotorVal = 0;
+	while (true) {
+		// target speed for the motor
+		word targetSpd = turnFlywheelOn? flywheelSpeed : 0;
+
+		// if it's greater than current, set it, otherwise "ramp down"
+		if (targetSpd >= fwMotorVal)
+			fwMotorVal = targetSpd;
+		else {
+			fwMotorVal--;
+			wait1Msec(30);
+		}
+
+		// set the motor value
+		motor[flywheel] = fwMotorVal;
+	}
+}
