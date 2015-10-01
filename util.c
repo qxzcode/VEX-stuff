1d1d400996e43935d8393ed2736a38a98287fd08
qxzcode <qxzcraft@gmail.com>
2015-10-01 16:56:38 -0700

    more work on PID control for flywheel speeds

--- util.c
+++ util.c
@@ -4,17 +4,20 @@ word flywheelSpeed = 80;
 struct pidState {
 	float kp, ki, kd;
 	float err, errDeriv, errSum;
-	float update(float error) {
-		errDeriv = error - err;
-		errSum += error;
-		err = error;
-		return kp*err + ki*errSum + kd*errDeriv;
-	}
 };
+float updatePID(pidState &pid, float error) {
+	pid.errDeriv = error - pid.err;
+	pid.errSum += error;
+	pid.err = error;
+	return pid.kp*pid.err + pid.ki*pid.errSum + pid.kd*pid.errDeriv;
+}
 
 task flywheelTask() {
 	// monitor turnFlywheelOn and control the motor safely
 	word fw1MotorVal = 0, fw2MotorVal = 0;
+	pidState pid1, pid2;
+	pid1.kp = pid2.kp = 1;
+	pid1.ki=pid1.kd=pid2.ki=pid2.kd = 0;
 	while (true) {
 		// target speed for the motor
 		word targetSpd = turnFlywheelOn? flywheelSpeed : 0;
@@ -33,6 +36,18 @@ task flywheelTask() {
 
 
 		// PID stuff
+
 		//flywheelPID(flywheel1, flywheelEnc1, targetSpd, fw1MotorVal);
+	}
+
+	float targetSpd;
+	const float kP = 1.0f;
+	while (true) {
+		float fw1Spd = (float)SensorValue[flywheelEnc1] / 50;
+		SensorValue[flywheelEnc1] = 0;
+
+		float P = kP*(targetSpd-fw1Spd)
+
+		wait1Msec(50);
 	}
 }
